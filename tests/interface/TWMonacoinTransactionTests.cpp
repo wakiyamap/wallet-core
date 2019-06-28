f54424600f9412f70b7603cd99eab9ee8234e5f6
wakiyamap <wakiyamap@gmail.com>
2019-06-29 08:10:47 +0900

    Recreate seed

--- tests/interface/TWMonacoinTransactionTests.cpp
+++ tests/interface/TWMonacoinTransactionTests.cpp
@@ -24,36 +24,36 @@ using namespace Bitcoin;
 TEST(MonacoinTransaction, SignTransaction) {
     /*
         https://iancoleman.io/bip39/
-        Mnemonic - raccoon slide close budget swarm benefit remind hard october coffee smoke whip
-        m/44'/22'/0'/0/0 Address - M8aShwteMWyAbUw4SGS4EHLqfo1EfnKHcM
-        m/44'/22'/0'/0/0 Private key in Base58 encoding - T6qjeygF1RubAq6wvd36U3hqgoBbW96tJEfTgEiGdEpWmwCuc5Lr
-        m/44'/22'/0'/0/0 Private key in bytes - 3e2ef03dbc272adea81284a9c12034e878f3bb5a95e08f4c06639f000374d79c
-        utxo - https://blockbook.electrum-mona.org/tx/3e2ef03dbc272adea81284a9c12034e878f3bb5a95e08f4c06639f000374d79c
-        tx - https://blockbook.electrum-mona.org/tx/79ebdce15e4ac933328e62dbe92302fc8b4833786e46df8a4f18295cb824fb67
+        Mnemonic - perfect runway tissue hover click expire orbit subway insane joy husband circle
+        m/44'/22'/0'/0/0 Address - MX7ZpcMMN4GVDeUvCjAYwfRyMgfBzYNr3E
+        m/44'/22'/0'/0/0 Private key in Base58 encoding - T8XV834egE6ZsgsQFPnBcYbNdFKNiGKiNj21mRjGx2scGNQh3ypZ
+        m/44'/22'/0'/0/0 Private key in bytes - a356a193a24c73c657e0c7bbf4e876964984a2dcba986ea1ea1fca7b025218f3
+        utxo - https://blockbook.electrum-mona.org/tx/167b642212c700a529d5d5fedd8ff9ec8641cb55a1806c007095102793b43286
+        tx - https://blockbook.electrum-mona.org/tx/cc01c378c9ce1c8218b1c311796eabc2c89b59fe535cd50d645ddb99eeba5593
     */
 
     const int64_t utxo_amount = 100000000;
     const int64_t amount = 50000000;
-    const int64_t fee = 40000;
+    const int64_t fee = 448000;
 
     auto input = Bitcoin::Proto::SigningInput();
     input.set_hash_type(TWSignatureHashTypeAll);
     input.set_amount(amount);
     input.set_byte_fee(1);
-    input.set_to_address("M9xFZzZdZhCDxpx42cM8bQHnLwaeX1aNja");
-    input.set_change_address("M8aShwteMWyAbUw4SGS4EHLqfo1EfnKHcM");
+    input.set_to_address("M8aShwteMWyAbUw4SGS4EHLqfo1EfnKHcM");
+    input.set_change_address("PDYLyFayFWSTjZZnxRKGDmmrrEpkpN1Nfq");
     input.set_coin_type(TWCoinTypeMonacoin);
 
-    auto hash0 = DATA("9cd77403009f63064c8fe0955abbf378e83420c1a98412a8de2a27bc3df02e3e");
+    auto hash0 = DATA("8632b49327109570006c80a155cb4186ecf98fddfed5d529a500c71222647b16");
     auto utxo0 = input.add_utxo();
     utxo0->mutable_out_point()->set_hash(TWDataBytes(hash0.get()), TWDataSize(hash0.get()));
     utxo0->mutable_out_point()->set_index(0);
     utxo0->mutable_out_point()->set_sequence(UINT32_MAX);
     utxo0->set_amount(utxo_amount);
-    auto script0 = parse_hex("76a9141685f46de8e94513da024c36c42cb6a9b5704ef188ac");
+    auto script0 = parse_hex("76a914076df984229a2731cbf465ec8fbd35b8da94380f88ac");
     utxo0->set_script(script0.data(), script0.size());
 
-    auto utxoKey0 = DATA("3e2ef03dbc272adea81284a9c12034e878f3bb5a95e08f4c06639f000374d79c");
+    auto utxoKey0 = DATA("a356a193a24c73c657e0c7bbf4e876964984a2dcba986ea1ea1fca7b025218f3");
     input.add_private_key(TWDataBytes(utxoKey0.get()), TWDataSize(utxoKey0.get()));
 
     auto plan = Bitcoin::TransactionBuilder::plan(input);
@@ -73,7 +73,7 @@ TEST(MonacoinTransaction, SignTransaction) {
     signedTx.encode(false, serialized);
     ASSERT_EQ(
         hex(serialized),
-        "02000000019cd77403009f63064c8fe0955abbf378e83420c1a98412a8de2a27bc3df02e3e000000006a473044022003ecf5c1d603743c5941684535fe1e9f6f4c15012c0193f22c4c77d758aea0e802205e9b675485adcee5d77260d69ad818a7b375f91738dffc3fc44ff2f6b2b0b8b7012102a5ab59fed49c92b5fe6f46e445ad9a6ac1410d7114d31917b286e65c942f3239feffffff024054fa02000000001976a914076df984229a2731cbf465ec8fbd35b8da94380f88ac80f0fa02000000001976a9141685f46de8e94513da024c36c42cb6a9b5704ef188ac5ef91900"
+        "02000000018632b49327109570006c80a155cb4186ecf98fddfed5d529a500c71222647b16000000006a47304402200511e81368a1c2f78815b4b6c531f88d238db7581daea67b0c55db145a7f2067022017647df16b3ea0434182bbcaa06c7bc397c933351bdcf25ae4a68dc4c58ade09012102fc08693599fda741558613cd44a50fc65953b1be797637f8790a495b85554f3efeffffff0280f0fa02000000001976a914076df984229a2731cbf465ec8fbd35b8da94380f88ac801af4020000000017a914364f93064cc3bd63811b540fc7a93562acef49b987fcf91900"
     ); 
 }
 
